3743aff67bb79aa48b89b41458ffafd626a41cad
AntoonAdel <antonadel359@gmail.com>
2024-04-12 03:03:07 +0200

    Done

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ The console, to manage everything
 """
 
 
+import cmd
+import sys
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,8 +14,6 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models import storage
-import cmd
-import sys
 
 
 class HBNBCommand(cmd.Cmd):
@@ -32,9 +32,9 @@ class HBNBCommand(cmd.Cmd):
         if '.' in line:
             HBNBCommand.__all_117 = 1
             line = line.replace('.', ' ').replace('(', ' ').replace(')', ' ')
-            cmd_argv = line.split()
-            cmd_argv[0], cmd_argv[1] = cmd_argv[1], cmd_argv[0]
-            line = " ".join(cmd_argv)
+            cmd_argvs = line.split()
+            cmd_argvs[0], cmd_argvs[1] = cmd_argvs[1], cmd_argvs[0]
+            line = " ".join(cmd_argvs)
         return cmd.Cmd.precmd(self, line)
 
     def do_quit(self, arg):
@@ -60,53 +60,53 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         "Print dict of a instance in base of it's ID"
-        cmd_argv = arg.split()
-        if not cmd_argv:
+        cmd_argvs = arg.split()
+        if not cmd_argvs:
             print("** class name missing **")
             return None
         try:
-            eval(cmd_argv[0])
+            eval(cmd_argvs[0])
         except:
             print("** class doesn't exist **")
             return None
 
-        all_objs = storage.all()
+        all_objects = storage.all()
 
-        if len(cmd_argv) < 2:
+        if len(cmd_argvs) < 2:
                 print("** instance id missing **")
                 return None
 
-        cmd_argv[1] = cmd_argv[1].replace("\"", "")
-        key = cmd_argv[0] + '.' + cmd_argv[1]
+        cmd_argvs[1] = cmd_argvs[1].replace("\"", "")
+        key = cmd_argvs[0] + '.' + cmd_argvs[1]
 
-        if all_objs.get(key, False):
-            print(all_objs[key])
+        if all_objects.get(key, False):
+            print(all_objects[key])
         else:
             print("** no instance found **")
 
     def do_all(self, arg):
         "Print all the instances saved in file.json"
-        cmd_argv = arg.split()
+        cmd_argvs = arg.split()
 
-        if cmd_argv:
+        if cmd_argvs:
             try:
-                eval(cmd_argv[0])
+                eval(cmd_argvs[0])
             except:
                 print("** class doesn't exist **")
                 return None
 
-        all_objs = storage.all()
+        all_objects = storage.all()
         print_list = []
-        len_objs = len(all_objs)
-        for key, value in all_objs.items():
-            if not cmd_argv:
+        len_objs = len(all_objects)
+        for key, value in all_objects.items():
+            if not cmd_argvs:
                 if HBNBCommand.__all_117 == 0:
                     print_list.append("\"" + str(value) + "\"")
                 else:
                     print_list.append(str(value))
             else:
                 check = key.split('.')
-                if cmd_argv[0] == check[0]:
+                if cmd_argvs[0] == check[0]:
                     if HBNBCommand.__all_117 == 0:
                         print_list.append("\"" + str(value) + "\"")
                     else:
@@ -119,101 +119,101 @@ class HBNBCommand(cmd.Cmd):
         "Deletes an instance based on it's ID and save the changes\n \
         Usage: destroy <class name> <id>"
 
-        cmd_argv = arg.split()
-        if not cmd_argv:
+        cmd_argvs = arg.split()
+        if not cmd_argvs:
             print("** class name missing **")
             return None
         try:
-            eval(cmd_argv[0])
+            eval(cmd_argvs[0])
         except:
             print("** class doesn't exist **")
             return None
 
-        all_objs = storage.all()
+        all_objects = storage.all()
 
-        if len(cmd_argv) < 2:
+        if len(cmd_argvs) < 2:
                 print("** instance id missing **")
                 return None
 
-        cmd_argv[1] = cmd_argv[1].replace("\"", "")
-        key = cmd_argv[0] + '.' + cmd_argv[1]
+        cmd_argvs[1] = cmd_argvs[1].replace("\"", "")
+        key = cmd_argvs[0] + '.' + cmd_argvs[1]
 
-        if all_objs.get(key, False):
-            all_objs.pop(key)
+        if all_objects.get(key, False):
+            all_objects.pop(key)
             storage.save()
         else:
             print("** no instance found **")
 
     def do_update(self, arg):
         "Usage: update <class name> <id> <attribute name> <attribute value>"
-        cmd_argv = []
-        part2_argv = []
+        cmd_argvs = []
+        part2_argvs = []
         is_dict = 0
         if "\"" in arg:
             if "," in arg:
                 if "{" in arg:
                     is_dict = 1
-                    part1_argv = arg.split(",")[0].split()
-                    for i in part1_argv:
-                        cmd_argv.append(i.replace("\"", ""))
-                    part2_argv = arg.replace("}", "").split("{")[1].split(", ")
-                    for i in part2_argv:
-                        for j in i.split(": "):
-                            cmd_argv.append(j.replace("\"", "")
+                    part1_argvs = arg.split(",")[0].split()
+                    for n in part1_argvs:
+                        cmd_argvs.append(n.replace("\"", ""))
+                    part2_argvs = arg.replace("}", "").split("{")[1].split(", ")
+                    for n in part2_argvs:
+                        for t in n.split(": "):
+                            cmd_argvs.append(t.replace("\"", "")
                                             .replace('\'', ""))
                 else:
                     arg_key = arg.replace(",", "")
-                    part1_argv = arg_key.split()
-                    for i in part1_argv[:2]:
-                        cmd_argv.append(i.replace("\"", ""))
-                    part2_argv = arg.split(", ")[1:]
-                    for i in part2_argv:
-                        cmd_argv.append(i.replace("\"", ""))
+                    part1_argvs = arg_key.split()
+                    for n in part1_argvs[:2]:
+                        cmd_argvs.append(n.replace("\"", ""))
+                    part2_argvs = arg.split(", ")[1:]
+                    for n in part2_argvs:
+                        cmd_argvs.append(n.replace("\"", ""))
             else:
-                part1_argv = arg.split("\"")[0]
-                for i in part1_argv.split():
-                    cmd_argv.append(i)
-                part2_argv = arg.split("\"")[1:]
-                for i in part2_argv:
-                    if i != " " and i != "":
-                        cmd_argv.append(i.replace("\"", ""))
+                part1_argvs = arg.split("\"")[0]
+                for n in part1_argvs.split():
+                    cmd_argvs.append(n)
+                part2_argvs = arg.split("\"")[1:]
+                for n in part2_argvs:
+                    if n != " " and n != "":
+                        cmd_argvs.append(n.replace("\"", ""))
 
         else:
-            part1_argv = arg.split()
-            for i in range(len(part1_argv)):
-                if i == 4:
+            part1_argvs = arg.split()
+            for n in range(len(part1_argvs)):
+                if n == 4:
                     break
-                cmd_argv.append(part1_argv[i])
+                cmd_argvs.append(part1_argvs[n])
 
-        if (len(cmd_argv) == 0):
+        if (len(cmd_argvs) == 0):
             print("** class name missing **")
             return None
 
         try:
-            eval(cmd_argv[0])
+            eval(cmd_argvs[0])
         except:
             print("** class doesn't exist **")
             return None
 
-        if len(cmd_argv) < 2:
+        if len(cmd_argvs) < 2:
             print("** instance id missing **")
             return None
 
-        all_objs = storage.all()
+        all_objects = storage.all()
 
-        key = cmd_argv[0] + '.' + cmd_argv[1]
-        if all_objs.get(key, False):
-            if (len(cmd_argv) >= 3):
-                if (len(cmd_argv) % 2) == 0:
-                    for i in range(2, len(cmd_argv), 2):
-                        attr = cmd_argv[i]
-                        type_att = getattr(all_objs[key], cmd_argv[i], "")
+        key = cmd_argvs[0] + '.' + cmd_argvs[1]
+        if all_objects.get(key, False):
+            if (len(cmd_argvs) >= 3):
+                if (len(cmd_argvs) % 2) == 0:
+                    for n in range(2, len(cmd_argvs), 2):
+                        attr = cmd_argvs[n]
+                        type_att = getattr(all_objects[key], cmd_argvs[n], "")
                         try:
-                            cast_val = type(type_att)(cmd_argv[i + 1])
+                            cast_val = type(type_att)(cmd_argvs[n + 1])
                         except:
                             cast_val = type_att
-                        setattr(all_objs[key], cmd_argv[i], cast_val)
-                        all_objs[key].save()
+                        setattr(all_objects[key], cmd_argvs[n], cast_val)
+                        all_objects[key].save()
                         if is_dict == 0:
                             break
                 else:
@@ -224,27 +224,27 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_count(self, arg):
-        "Usage: count <class name> or <class name>.count()"
-        cmd_argv = arg.split()
+        "Usage: counter <class name> or <class name>.counter()"
+        cmd_argvs = arg.split()
 
-        if cmd_argv:
+        if cmd_argvs:
             try:
-                eval(cmd_argv[0])
+                eval(cmd_argvs[0])
             except:
                 print("** class doesn't exist **")
                 return None
 
-        all_objs = storage.all()
-        count = 0
+        all_objects = storage.all()
+        counter = 0
 
-        for key, value in all_objs.items():
-            if not cmd_argv:
-                count += 1
+        for key, value in all_objects.items():
+            if not cmd_argvs:
+                counter += 1
             else:
                 check = key.split('.')
-                if cmd_argv[0] == check[0]:
-                    count += 1
-        print(count)
+                if cmd_argvs[0] == check[0]:
+                    counter += 1
+        print(counter)
 
 
 if __name__ == '__main__':
